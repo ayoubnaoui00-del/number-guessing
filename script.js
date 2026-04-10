ea7f32ef9df0040152fa64dd9b7f47a02be6f0a9
ayoubnaoui00-del <ayoubnaoui00@gmail.com>
2026-04-10 12:44:21 +0100

    made changes on html and js

---
+++ script.js
@@ -0,0 +1,21 @@
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+let guesses = document.querySelector('.guesses')
+let lastResult = document.querySelector('.lastResult');
+let lowOrHigh = document.querySelector('.lowOrHigh'  );
+let guessSubmit = document.querySelector('.guessSubmit')
+let guessField = document.querySelector('.guessField')
+let guessCount = 1;
+let resetbutton;
+
+guessField.focus()
+function checkGuess () {
+    let userGuess = Number(guessField.value);
+    if(guessCount === 1){
+        guess.textContent = 'Previous Guesses: '; 
+    }
+    guess.textContent += userGuess + ' ';
+    if(userGuess === randomNumber) {
+        lastResult.textContent ='congratulations! you got it right!'
+         lastResult.style.backgroundColor= 'green'
+    }
+}
